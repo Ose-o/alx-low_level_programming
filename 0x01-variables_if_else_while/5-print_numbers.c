3f241e6cce724fcd88163656d61c6b5753b88c53
Oseo <Oriabureo@yahoo.co.uk>
2023-03-17 18:05:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+printf("\x");
+return (0);
+}
