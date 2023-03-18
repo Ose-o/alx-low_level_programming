31ac14719c855dbfd887333a37d235bad50d8fec
Oseo <Oriabureo@yahoo.co.uk>
2023-03-17 19:43:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main -Print a line of code using puts
+/**
+ *  main -Entry point
  *
- * Return:0
-*/
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
