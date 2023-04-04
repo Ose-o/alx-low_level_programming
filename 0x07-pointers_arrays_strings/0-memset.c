233baf646388d4bb3afeea3daf2652b677c8f879
Oseo <Oriabureo@yahoo.co.uk>
2023-04-03 18:11:42 -0700

    O-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
 #include "main.h"
-/* function fills the first n
+/**
+ * function fills the first n
  * bytes of the memory area pointed to by s
  * with constant byte b
  * Returns a pointer to the memory area s
- * Author @Ose O
+ * Author @Ose
+ * Return: a pointer to the memory area @s
  */
-
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+
 	while (i < n)
 	{
-		s[i] = b;
-		i++;
+	s[i] = b;
+	i++;
 	}
+
 	return (s);
 }
