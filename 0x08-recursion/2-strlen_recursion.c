5fcceef60caa6c08821ed0acd1f28aaf85e7190e
Oseo <Oriabureo@yahoo.co.uk>
2023-04-06 16:24:15 -0700

    Task 3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s
+ * Return:int
+ * Author:Ose O
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
+
