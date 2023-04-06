d0085fab9153c6508e99cba77c8ae84c8aef7fd9
Oseo <Oriabureo@yahoo.co.uk>
2023-04-05 17:32:21 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
 /**
- * Return: Alway
- *
- * _puts_recursion
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ * Author: Ose Oriabure
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
