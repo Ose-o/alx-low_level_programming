66f232aab3806a7dd65389a37508857818edd1f8
Oseo <Oriabureo@yahoo.co.uk>
2023-04-05 18:31:33 -0700

    Task 3

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ * Author Ose Oriabure
+ * Return: int
+ */
+int check(int c, int d)
+{
+	if (c * c == d)
+		return (c);
+	if (c * c > d)
+		return (-1);
+	return (check(c + 1, d));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}
