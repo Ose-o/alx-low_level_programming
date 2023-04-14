dbf8f882ebefde862bb3bd0ae4e5b06a3c7ef237
Oseo <Oriabureo@yahoo.co.uk>
2023-04-13 18:51:01 -0700

    Task Meall

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,7 +6,7 @@
  * @ptr: pointer to the memory previously allocated
  * @old_size:is the size, in bytes, of the allocated space for ptr
  * @new_size: the new size, in bytes of the new memory block
- * Aurthor:Ose Oriabre 
+ * Aurthor:Ose Oriabre
  * Return: pointer allocate new size memory, or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
