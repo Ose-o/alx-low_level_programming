cc31b32f8822aa1b6bed8925cf830c6781280abf
Oseo <Oriabureo@yahoo.co.uk>
2023-04-18 08:54:20 -0700

    remaining commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - print the name of the file
- *
- * Return: always 0
- */
+ * main - prints the name of the file
+ * Aurthor
+ *Return: always 0
+*/
 
 int main(void)
 {
-
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
