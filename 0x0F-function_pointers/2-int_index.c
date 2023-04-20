524cf3a9ed30d7265768fb74918a9cd37cbd801c
Oseo <Oriabureo@yahoo.co.uk>
2023-04-19 18:21:27 -0700

    commit 0

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index
+ *
+ *
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if  (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
