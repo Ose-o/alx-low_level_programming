00f79d01a59ad799fcfaf15305eb4ca2c89ee94b
Oseo <Oriabureo@yahoo.co.uk>
2023-04-29 07:43:26 -0700

    singly linked

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,16 @@
  * @
  *
  * Author: Ose Oriabure
-void free_list(list_t *head) {
+ */
+void free_list(list_t *head)
+{
     list_t *voltage = head;
     list_t *next;
 
-    while (voltage != NULL) {
+    while (voltage != NULL)
+    {
     next = voltage->next;
     free(voltage);
     voltage = next
     }
-    }
+}
