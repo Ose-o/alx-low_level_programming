082b064421d1f977787b0c4767c0ce15f7fc334f
Oseo <Oriabureo@yahoo.co.uk>
2023-04-30 22:19:40 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,16 +4,13 @@
 /**
  * print_listint - prints all the elements of a listint_t list.
  *
- * 
  * @h: head of linklist node
  * Return: the number of nodes
  * Author: Ose Oriabure
  */
 size_t print_list(const list_t *h)
 {
-	
 	size_t node_count = 0;
-
 	while (h != NULL)
 	{
 		printf("%d\n", h->distr);
