9fb8285115565d4c01ec03aca00d8bfb6096635c
Oseo <Oriabureo@yahoo.co.uk>
2023-04-30 22:32:31 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
  * Return: the number of nodes
  * Author: Ose Oriabure
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 0;
 	while (h != NULL)
 	{
-		printf("%d\n", h->distr);
+		printf("%d\n", h->n);
 		h = h->next;
 		node_count++;
 	}
