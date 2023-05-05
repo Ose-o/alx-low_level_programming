3ce9c578162156758a20cbc2c3163b39f140c423
Oseo <Oriabureo@yahoo.co.uk>
2023-05-05 15:47:08 -0700

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,24 +3,25 @@
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
+ * Author:Ose o
  */
-void print_binary(unsigned long int num)
+void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	int i, volt = 0;
+	unsigned long int volt;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = num >> i;
+		volt = n >> i;
 
-		if (current & 1)
+		if (volt & 1)
 		{
 			_putchar('1');
-			count++;
+			volt++;
 		}
-		else if (count)
+		else if (volt)
 			_putchar('0');
 	}
-	if (!count)
+	if (!volt)
 		_putchar('0');
 }
