a43d15f42ffb28a10d9431a44691ff1912b52b50
Oseo <Oriabureo@yahoo.co.uk>
2023-05-04 19:35:47 -0700

    Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (i > 63)
+	if (index > 63)
 		return (-1);
 
-	*n = (~(1UL << i) & *n);
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
-
